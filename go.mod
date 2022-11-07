5469f86e681170bffb89c6e999ceaaedbd259983
Aleksandr Sapozhnikov <alexxandrsap@mail.ru>
2022-11-07 22:44:12 +0300

    add Game struct

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module tetris
 
 go 1.18
+
+require github.com/AlexxSap/matrix v1.0.0 // indirect
--- tetris/game.go
+++ tetris/game.go
@@ -1,7 +1,38 @@
 package tetris
 
-import "fmt"
+import (
+	"github.com/AlexxSap/matrix"
+)
+
+type Game struct {
+	rowCount    int
+	columnCount int
+	field       *matrix.Matrix[int]
+}
+
+func newGame() *Game {
+	rowCount := 20
+	columnCount := 10
+	d := make([]int, rowCount*columnCount, rowCount*columnCount)
+
+	m, err := matrix.NewMatrix[int](d, rowCount, columnCount)
+	if err != nil {
+		panic(err)
+	}
+
+	return &Game{
+		rowCount:    rowCount,
+		columnCount: columnCount,
+		field:       m,
+	}
+}
+
+func (gm *Game) repaint() {
+
+}
 
 func Start() {
-	fmt.Println("start")
+
+	game := newGame()
+	go game.repaint()
 }
